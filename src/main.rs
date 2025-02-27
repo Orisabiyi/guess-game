5748e71e7ea8a85adcf546e6e7261d2932e145d2
Orisabiyi <orisabiyidavid@gmail.com>
2025-02-27 09:12:21 +0100

    add random number generation for guess-game functionality

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
-
     println!("Provide your guess");
+
+    // generate random secret number
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is {}", secret_number);
 
     // declare a variable to hold users input
     let mut guess = String::new();
