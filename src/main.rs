265eefbb4115bf6f4e5f94ebbb8ff360e326e8e8
Orisabiyi <orisabiyidavid@gmail.com>
2025-02-28 13:37:11 +0100

    refactor: implement input loop for continuous guessing and improve output formatting

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,18 @@ fn main() {
     println!("Guess the number");
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new(); // a mutable variable that holds a string value
-    io::stdin().read_line(&mut guess).expect("Failed to read line "); // getting user input
-    
-    let guess: u32= guess.trim().parse().expect("Provide a number");
-    
-    println!("You guessed {}", guess);
+    loop {
+        let mut guess = String::new(); // a mutable variable that holds a string value
+        io::stdin().read_line(&mut guess).expect("Failed to read line "); // getting user input
+        
+        let guess: u32= guess.trim().parse().expect("Provide a number");
+        
+        println!("You guessed {} \n", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => print!("You won!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => print!("You won!"),
+        }
     }
 }
